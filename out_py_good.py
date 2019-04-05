3ec7d5542006a3295fb9e3e3505d87e845891575
Yl0w0lY <B04902109@ntu.edu.tw>
2019-04-06 03:47:48 +0800

    Add files via upload

---
+++ out_py_good.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#     
+diff = '''H«!˜Zí$¹ÑñnÇ±Ö–„ù`Wó?Ş@î…ûÃÁ¸ñê¾üFK"]ô¸µ–;hjN®JY°ëïäv€ôâ+¯:¦gº%¬";’Îê½`"Õ»öÀTay‘s^º·‚u™Yµ-ÊbèÊùY¦3s˜T…ºD°æ'''
+same = '''H«!˜Zí$¹ÑñnÇ±Ö–„ù`Wó?Ş@î…ûÃÁ¸ñê¾üFK"]ô¸µ–;hjN®JY°ëïäv€ôâ+¯:¦gº%¬";’Îê½`"Õ»öÀTay‘s^º·‚u™Yµ-ÊbèÊùY¦3s˜T…ºD°æ'''
+
+if (same == diff):
+    print "MD5 is secure!"
+
+else:
+    print "Just kidding!"
+
